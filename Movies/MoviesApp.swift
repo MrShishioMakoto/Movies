eaa018b89753d619ce3dd19319776b8f4646e625
goncaloalmeida <goncaloalmeida>
2024-03-11 16:36:57 +0000

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by goncaloalmeida on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
